3871a85736d3cab00fcaa2c21d48c88ef4e65312
denis <den_vbg@mail.ru>
2022-12-20 21:37:51 +0300

    add homework 2 seminar

---
+++ task06/main2.py
@@ -0,0 +1,18 @@
+# Задайте список из n чисел последовательности (1 + 1/n)^n, выведите его на экран, а так же сумму элементов списка.
+# Пример:
+# Для n=4 -> [2, 2.25, 2.37, 2.44]
+# Сумма 9.06
+
+# n = abs(int(input("Введите число: ")))
+# list = []
+# for i in range(n):
+#     number = float((1 + 1/n)**n)
+#     list.append(number)
+#     sum += number
+# print(f"Сумма элементов {sum}")
+# print(list)
+
+list1 = ['hi', 'welcome']
+list2 = list1
+list2.append('bro')
+print(list1[-1])
